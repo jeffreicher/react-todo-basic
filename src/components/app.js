aaea292fbfb0c21498753d394f568471b2239a7f
Jeffrei Cher <jeffreicher@yahoo.com>
2018-03-29 14:37:46 -0700

    FS2 completed. added ability to add list items to list array

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,33 @@
-import React from 'react';
 import List from './list';
+import AddForm from './add_form';
+import React, { Component } from 'react';
+import ListData from '../data/todo_items';
 import 'materialize-css/dist/css/materialize.min.css';
 
-const App = () => (
-    <div className="container">
-        <h1 className="center">To Do List</h1>
-        <List />
-    </div>
-);
+class App extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            list: ListData
+        };
+    };
+
+    addItem(item) {
+        this.setState({
+            list: [item, ...this.state.list]
+        });
+    };
+
+    render() {
+        return (
+            <div className="container">
+                <h1 className="center">To Do List</h1>
+                <AddForm add={this.addItem.bind(this)}/>
+                <List list={this.state.list}/>
+            </div>
+        );
+    };
+};
 
 export default App;
